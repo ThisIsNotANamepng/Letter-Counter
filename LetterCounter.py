af636c870f8111dc99ca9089a9e75cbcf41bc9bc
ThisIsNotANamepng <74005486+ThisIsNotANamepng@users.noreply.github.com>
2021-04-08 20:24:03 -0500

    Add files via upload

---
+++ LetterCounter.py
@@ -0,0 +1,82 @@
+measure = input("Enter ")
+
+a = measure.count("a") 
+print ("A-", a)
+
+b = measure.count("b")
+print ("B-", b)
+
+c = measure.count("c")
+print ("C-", c)
+
+d = measure.count("d")
+print ("D-", d)
+
+e = measure.count("e")
+print ("E-", e)
+
+f = measure.count("f")
+print ("F-", f)
+
+g = measure.count("g")
+print ("G-", g)
+
+h = measure.count("h")
+print ("H-", h)
+
+i = measure.count("i")
+print ("I-", i)
+
+j = measure.count("j")
+print ("J-", j)
+
+k = measure.count("k")
+print ("K-", k)
+
+l = measure.count("l")
+print ("L-", l)
+
+m = measure.count("m")
+print ("M-", m)
+
+n = measure.count("n")
+print ("N-", n)
+
+o = measure.count("o")
+print ("O-", o)
+
+p = measure.count("p")
+print ("P-", p)
+
+q = measure.count("q")
+print ("Q-", q)
+
+r = measure.count("r")
+print ("R-", r)
+
+s = measure.count("s")
+print ("S-", s)
+
+t = measure.count("t")
+print ("T-", t)
+
+u = measure.count("u")
+print ("U-", u)
+
+v = measure.count("v")
+print ("V-", v)
+
+w = measure.count("w")
+print ("W-", w)
+
+x = measure.count("x")
+print ("X-", x)
+
+y = measure.count("y")
+print ("Y-", y)
+
+z = measure.count("z")
+print ("Z-", z)
+
+
+
